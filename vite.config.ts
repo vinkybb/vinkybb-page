245c471a1ac4ddd5f01ecb93205b363f638f1864
vinkybb <1960604248@qq.com>
2026-03-23 23:23:31 +0800

    chore: configure github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
-  const base = env.VITE_GITHUB_PAGES_BASE || '/';
+  const base = env.VITE_GITHUB_PAGES_BASE || '/vinkybb-page/';
 
   return {
     base,
